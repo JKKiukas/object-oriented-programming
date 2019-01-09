dae172012b2388bed215dc2b0b666660438c81a3
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-01-09 09:04:29 +0200

    Book class task done.

---
+++ Kirja luokka/Kirja luokka/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kirja_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            BookStore firstBook = new BookStore("Pyörre", "Ilkka Remes", 111000111, 29.95);
+            firstBook.BookInfo();
+
+            BookStore secondBook = new BookStore("Puhdistus", "Sofi Oksanen", 222000222, 19.95);
+            secondBook.BookInfo();
+
+            Console.WriteLine(firstBook.CompareBooks(secondBook));
+        }
+    }
+}
