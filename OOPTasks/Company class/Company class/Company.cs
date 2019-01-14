fcb4f37af9e4493a632f3493ffc29ad892534f73
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-01-14 15:26:23 +0200

    Company class task done.

---
+++ OOPTasks/Company class/Company class/Company.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Company_class
+{
+    class Company
+    {
+        public string title;
+        public string address;
+        public string phone;
+        public int income;
+        public int expense;
+
+        public Company(string title, string address, string phone, int income, int expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.income = income;
+            this.expense = expense;
+        }
+        public void CompanyInfo()
+        {
+            Console.WriteLine($"Yhtiön nimi: {this.title}");
+            Console.WriteLine($"Yhtiön osoite: {this.address}");
+            Console.WriteLine($"Yhtiön puhelinnumero: {this.phone}");
+            Console.WriteLine($"Yhtiön tulot: {this.income}€");
+            Console.WriteLine($"Yhtiön menot: {this.expense}€\n");
+        }
+
+        public Company()
+        {
+            this.title = "Pertin antiikki Oy";
+            this.address = "Vanhatie 10, 54100 Lappeenranta";
+            this.phone = "050987654";
+            this.income = 100000;
+            this.expense = 30000;
+        }
+
+        public Company(Company previousCompany)
+        {
+            this.title = previousCompany.title;
+            this.address = previousCompany.address;
+            this.phone = previousCompany.phone;
+            this.income = previousCompany.income;
+            this.expense = previousCompany.expense;
+        }
+
+        public void Profit()
+        {
+            double profit = 100 * (this.income - this.expense) / this.expense;
+            Console.WriteLine($"Yhtiön {this.title}:n tekemä voitto on {profit}%.");
+
+            if (profit > 300)
+            {
+                Console.WriteLine($"Yhtiön taloudellinen tilanne on hyvä.\n");
+            }
+            else if(profit < 100)
+            {
+                Console.WriteLine($"Yhtiön taloudellinen tilanne on huono.\n");
+            }
+            else
+            {
+                Console.WriteLine($"Yhtiön taloudellinen tilanne on kohtalainen.\n");
+            }
+        }
+    }
+}
---
+++ OOPTasks/Company class/Company class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Company_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma kertoo tietoja käyttäjälle eri yhtiöstä ja niiden taloudellisesta tilanteesta.\n");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Company firstCompany = new Company("Joukon autokorjaamo Oy", "Ruuvikuja 2, 53100 Lappeenranta", "040123456", 25000, 20000);
+            Company secondCompany = new Company();
+            //Adds copy constructor. Copies first company's info.
+            Company thirdCompany = new Company(firstCompany);           
+
+            firstCompany.CompanyInfo();
+            secondCompany.CompanyInfo();
+            thirdCompany.CompanyInfo();
+
+            //Changes first company's info.
+            thirdCompany.title = "Matin kultakello Oy";
+            thirdCompany.address = "Arvotie 18, 53100 Lappeenranta";
+            thirdCompany.phone = "040061938";
+            thirdCompany.income = 350000;
+            thirdCompany.expense = 75000;
+
+            thirdCompany.CompanyInfo();
+
+            firstCompany.Profit();
+            secondCompany.Profit();
+            thirdCompany.Profit();
+        }
+    }
+}
