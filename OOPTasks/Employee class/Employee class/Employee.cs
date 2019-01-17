ea9f069ef0560f658684a86ac18da74b8f3e6e3e
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-01-17 13:48:19 +0200

    Employee class task done.

---
+++ OOPTasks/Employee class/Employee class/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_class
+{
+    class Employee
+    {
+        public string name;
+        public string id;
+        public string position;
+        public double salary;
+
+        public Employee(string name, string id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"Työntekijän nimi: {this.name}\nTyöntekijän HeTu: {this.id}\nTyöntekijän tehtävä: {this.position}\nTyöntekijän palkka: {this.salary}€/kk\n";
+        }
+
+        public string CompareSalary(Employee employee)
+        {
+
+            if (salary > employee.salary)
+            {
+                return ($"Työntekijän {this.name} palkka {this.salary}€/kk on suurempi, kuin työntekijän {employee.name} palkka {employee.salary}€/kk.");
+            }
+            else if (salary < employee.salary)
+            {
+                return ($"Työntekijän {employee.name} palkka {employee.salary}€/kk on suurempi, kuin työntekijän {this.name} palkka {this.salary}€/kk.");
+            }
+            else if (salary < employee.salary)
+            {
+                return ($"Työntekijän {this.name} palkka {this.salary}€/kk on suurempi, kuin työntekijän {employee.name} palkka {employee.salary}€/kk.");
+            }
+            else
+                return ($"Työntekijän {this.name} palkka {this.salary}€/kk on yhtäsuuri, kuin työntekijän {employee.name} palkka {employee.salary}€/kk.");
+        }
+    }
+}
---
+++ OOPTasks/Employee class/Employee class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Employee_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa työntekijöiden tiedot ja vertailee palkkoja.\n");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Employee[] employees = new Employee[4];
+            employees[0] = new Employee("Jouko", "271068-893L", "Autonasentaja", 2300);
+            employees[1] = new Employee("Pertti", "030872-375D", "Yksityisyrittäjä", 2500);
+            employees[2] = new Employee("Matti", "170474-751T", "Kelloseppä", 2800);
+            employees[3] = new Employee("Kalevi", "100676-239U", "Puuseppä", 2300);
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.ToString());
+            }
+
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+            Console.WriteLine(employees[0].CompareSalary(employees[2]));
+            Console.WriteLine(employees[2].CompareSalary(employees[1]));
+            Console.WriteLine(employees[0].CompareSalary(employees[3]));
+        }
+    }
+}
