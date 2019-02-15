66a1dcaa0afe014e3232583062e7c3f34bc72032
Jonne Kiukas <jonne.kiukas@hotmail.com>
2019-02-15 16:32:36 +0200

    Polygon task done.

---
+++ OOPTasks/Polygon/Polygon/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polygon
+{
+    class Triangle : Shape
+    {
+        public double length;
+        public double width;
+
+        public Triangle()
+        {
+            length = 0;
+            width = 0;
+        }
+
+        public Triangle(string type, string color, double length, double width)
+            :base(type, color)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double Area()
+        {
+            return length * width / 2;
+        }
+
+        public override string GetInfo()
+        {
+            return $"Muoto: {type}"+
+                   $"\nVäri: {color}"+
+                   $"\nKolmion pituus: {length} cm" +
+                   $"\nKolmion leveys: {width} cm" +
+                   $"\nKolmion pinta-ala: {Area()} cm";
+        }
+    }
+}
